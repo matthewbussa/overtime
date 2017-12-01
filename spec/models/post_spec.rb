db97f38c9b66600c443e2f9b06e0c9ba40e5ccae
Matthew Bussa <matthewbussa@gmail.com>
2017-12-01 15:31:13 -0500

    refactored to include factory girl factories

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "creation" do 
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @user = FactoryGirl.create(:user)
+      @post = FactoryGirl.build_stubbed(:post)
     end
 
     it "can be created" do
