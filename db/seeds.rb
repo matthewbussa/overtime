30739fd53ca3cf926365e9ca844a5d0f9d08b1d9
Matthew Bussa <matthewbussa@gmail.com>
2017-12-12 15:11:08 -0500

    custom db query by scope

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,13 @@
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+@admin_user = AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+
+2.times do |post|
+    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @admin_user.id)
+end
+
+puts "2 admin posts have been created"
 
 100.times do |post|
     Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
