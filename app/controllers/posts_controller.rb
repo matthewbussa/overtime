188f76a14301d0b84d81aea7300522a5bac1efe6
Matthew Bussa <matthewbussa@gmail.com>
2017-12-12 11:26:01 -0500

    added pundit policy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,51 +1,53 @@
 class PostsController < ApplicationController
-    before_action :set_post, only: [:show, :edit, :update, :destroy]
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @posts = Post.all 
-    end
+	def index
+			@posts = Post.all 
+	end
 
-    def new
-        @post = Post.new 
-    end
+	def new
+			@post = Post.new 
+	end
 
-    def create
-        @post = Post.new(post_params)
-        @post.user_id = current_user.id
-        if @post.save
-            redirect_to @post, notice: 'Your post was created successfully'
-        else 
-            render :new
-        end        
-    end
+	def create
+			@post = Post.new(post_params)
+			@post.user_id = current_user.id
+			if @post.save
+					redirect_to @post, notice: 'Your post was created successfully'
+			else 
+					render :new
+			end        
+	end
 
-    def show
-    end
+	def show
+	end
 
-    def destroy
-			@post.delete
-    	redirect_to posts_path, notice: 'Your post was deleted'
-    end
+	def destroy
+		@post.delete
+		redirect_to posts_path, notice: 'Your post was deleted'
+	end
 
-    def edit 
+	def edit
+		authorize @post
+	end
+
+	def update
+		authorize @post
+
+		if @post.update(post_params)
+			redirect_to @post, notice: 'Your post was edited successfully'
+		else
+			render edit
 		end
+	end
 
-		def update
-			if @post.update(post_params)
-				redirect_to @post, notice: 'Your post was edited successfully'
-			else
-				render edit
-			end
+	private
 
-		end
+	def post_params
+		params.require(:post).permit(:date, :rationale, :status)
+	end
 
-    private
-
-        def post_params
-            params.require(:post).permit(:date, :rationale, :status)
-        end
-
-        def set_post
-            @post = Post.find(params[:id])
-        end
+	def set_post
+		@post = Post.find(params[:id])
+	end
 end
