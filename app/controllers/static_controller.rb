b4a5266a8ea12058bb47ed80c76d7ee599204635
Matthew Bussa <matthewbussa@gmail.com>
2017-12-18 12:47:27 -0500

    implemented functional buttons for employee confirmation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,8 +4,8 @@ class StaticController < ApplicationController
 				@pending_approvals = Post.submitted
 				@recent_audit_logs = AuditLog.last(10)
 			else
-				@pending_audit_confirmations = current_user.audit_logs
+				#TODO Fix Scope
+				@pending_audit_confirmations = current_user.audit_logs.pending
 			end
-
     end
 end
