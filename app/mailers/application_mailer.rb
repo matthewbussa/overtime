8f5d0576e24987bd2655b905fc94a365855e09c0
Matthew Bussa <matthewbussa@gmail.com>
2017-12-18 21:23:51 -0500

    configured email with mailgun

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'mailer@domain.com'
+  default from: 'postmaster@sandboxa4d345fcb0fd437d9d83dd42dc47bc1b.mailgun.org'
   layout 'mailer'
 end
